ca292d811f8c9947d696fad88583da9aeb30eb94
Luis Fernando Hernandez <hluisfernando1@gmail.com>
2021-08-03 08:17:44 -0500

    init app

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+// importacion 
+import React from 'react'
+import Result from './components/Result'
+
+import './App.css'
+
+const App = () => {
+    console.log("Renderización de App")
+    return (
+    <main className='react-calculator'>
+        <Result value={"0"}/>
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>clear</button>
+            <button>remove</button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
+    </main>)
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+//componente funcional 
+const Button = () => {
+    return "Button"
+}
+
+
+export default Button
